a557c747034e0ec18bd53d001235224711614196
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2023-06-27 19:46:02 +0100

    kafka consumer

--- src/main/java/com/toskafx/kafkaservice/kafka/KafkaConsumer.java
+++ src/main/java/com/toskafx/kafkaservice/kafka/KafkaConsumer.java
@@ -1,2 +1,16 @@
-package com.toskafx.kafkaservice.kafka;public class KafkaConsumer {
+package com.toskafx.kafkaservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = {"new-user"}, groupId = "myGroup")
+    public void consumerMessages(String message){
+        LOG.info("Message received is -> {}", message);
+    }
 }
