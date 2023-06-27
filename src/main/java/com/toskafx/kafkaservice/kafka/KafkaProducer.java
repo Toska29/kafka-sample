1046c5a2bcbf7c0cab0d90e9d38a6ac0da9723f9
Tosin Akinnusi <akinnusitosinbenedict@gmail.com>
2023-06-27 18:13:06 +0100

    kafka service - producer and consumer

---
+++ src/main/java/com/toskafx/kafkaservice/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.toskafx.kafkaservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        LOG.info("Message sent {}", message);
+        kafkaTemplate.send("new-user", message);
+    }
+}
